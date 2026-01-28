b3c66f018fff3292b3e095c398b6db17cacba12a
Rachel McLeod <128703392+mcldrchl@users.noreply.github.com>
2026-01-28 16:44:13 +0100

    R shiny initial commit

---
+++ 26_02_03_Rshiny/exploring-wastewater/app.R
@@ -0,0 +1,86 @@
+# add required libraries here
+library(shiny)
+library(dplyr)
+library(bslib)
+library(bsicons)
+library(ggplot2)
+
+# Define UI for application
+ui <- page_fluid(
+
+  navset_bar( # creates menu bar
+
+    title = "Exploring Wastewater",
+    id = "main_nav",
+
+    nav_panel(
+      title = "Viruses", # title displayed in the ui
+      value = "viruses", # id for referring to the panel
+      icon = bs_icon("virus"), # an icon displayed in the ui
+      # the code for what will be displayed
+      card(
+        card_header("SARS-CoV-2"),
+        layout_sidebar(
+          sidebar = sidebar(
+            # check box input for showing individual data points or not
+            checkboxInput(
+              "show_points",
+              label = "Show data points"
+            ),
+            uiOutput("wwtp_select_ui")
+          ),
+          # main card content here
+          plotOutput("SARS_plot")
+        )
+      )
+    ),
+
+    selected = "viruses",
+    bg = "#46ACC8",
+    fillable = FALSE,
+    position = "fixed-top" # defines menu bar position
+
+  ),
+
+  theme = bs_theme() %>%
+    bs_add_rules("body { padding-top: 50px !important }"), # adds space between navbar and first card
+  title = "Exploring Wastewater"
+
+)
+
+# Define server logic
+server <- function(input, output) {
+
+  PCRdata <- readr::read_csv("data/PCRdata.csv")
+
+  # dropdown menu input for choosing wastewater treatment plants to show
+  output$wwtp_select_ui <- renderUI({
+
+    selectizeInput("wwtp_select",
+                   label = "Wastewater treatment plant",
+                   choices = unique(PCRdata$wastewater_treatment_plant.name),
+                   selected = unique(PCRdata$wastewater_treatment_plant.name),
+                   multiple = TRUE)
+  })
+
+  # plot for output
+  output$SARS_plot <- renderPlot({
+
+    PCRdata %>%
+      filter(target == "SARS-N1",
+             wastewater_treatment_plant.name %in% input$wwtp_select) %>%
+      ggplot(aes(x = collection_date,
+                 color = wastewater_treatment_plant.name)) +
+      geom_line(aes(y = load_median)) +
+      {if(input$show_points) geom_point(aes(y = load))} +
+      labs(x = NULL,
+           y = "Viral load [gc/person/day]",
+           color = "Wastewater Treatment Plant") +
+      theme_minimal()
+
+  })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ 26_02_03_Rshiny/exploring-wastewater/retrieve-data.R
@@ -0,0 +1,76 @@
+#################################################################################
+# Script for retrieving data from the WISEdb - this is slightly slow so the idea
+# is to speed things up for the workshop.
+#
+# Run this script once before working with the app.
+#################################################################################
+
+# Function to retrieve data from the WISEdb
+get_data <- function(url, accept) {
+  full_url <- URLencode(url)
+  if (stringr::str_detect(accept, "csv")) {
+    csv <- RCurl::getURL(full_url, httpheader = c(Accept = accept))
+    df <- data.table::fread(text = csv, sep = ",", header = TRUE, stringsAsFactors = FALSE)
+    if (nrow(df) == 0) {
+      warning(paste("No data received from", url))
+    } else {
+      df <- df[, -1] # Return w/o index
+    }
+  } else if (stringr::str_detect(accept, "json")) {
+    json <- RCurl::getURL(full_url, httpheader = c(Accept = accept), .encoding = "UTF-8")
+    df <- jsonlite::parse_json(json, simplifyVector = TRUE)
+    if (length(df) == 0) {
+      warning(paste("No data received from", url))
+    } else {
+      df <- df[, -1] # Return w/o index
+    }
+  }
+  return(df)
+}
+
+# Configure API for reading data
+api <- "https://wisedb.ethz.ch/api/"
+
+# URls for data after the ? are filtering conditions
+pcr_url <- paste0(api, "public/export/dpcr_filtered_averaged/?assay=RESPV6&from=2024-01-01&to=2026-01-01")
+wwtp_url <- paste0(api, "public/metadata/wastewater_treatment_plant/?sampling=True")
+re_url <- paste0(api, "public/export/r_estimation/?from=2024-01-01&to=2026-01-01")
+
+# Load PCR data from API
+PCRdata <- get_data(pcr_url, "text/csv") %>%
+  mutate(
+    collection_date = lubridate::ymd(collection_date)
+  ) %>%
+  group_by(wastewater_treatment_plant.name, target) %>%
+  tidyr::complete(collection_date = tidyr::full_seq(collection_date, period = 1),
+                  fill = list(load = NA)) %>%
+  mutate(load_median = zoo::rollapply(load,
+                                 width = 7,
+                                 FUN = function(x) median(x, na.rm = TRUE),
+                                 align = "center",
+                                 fill = TRUE,
+                                 partial = TRUE)) %>%
+  ungroup()
+
+# Write PCR data to csv file
+if (dim(PCRdata)[1] != 0) {
+  readr::write_csv(PCRdata, "26_02_03_Rshiny/exploring-wastewater/data/PCRdata.csv")
+}
+
+# Load wwtp info from API
+wwtps <- get_data(wwtp_url, "application/json") %>%
+  mutate(across(.cols = c("canton", "name"), .fns = factor))
+
+# Write wwtp info to csv file
+if (dim(wwtps)[1] != 0) {
+  readr::write_csv(wwtps, "26_02_03_Rshiny/exploring-wastewater/data/wwtps.csv")
+}
+
+# Load Re data from API
+Redata <- get_data(re_url, "text/csv") %>%
+  mutate(date = lubridate::ymd(date))
+
+# Write Re data to csv file
+if (dim(Redata)[1] != 0) {
+  readr::write_csv(Redata, "26_02_03_Rshiny/exploring-wastewater/data/Redata.csv")
+}
